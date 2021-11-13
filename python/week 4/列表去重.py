702ef6c35bf57740fbb92c2871ff8e6aaf33d361
renxiao <92198500+Benxaomin@users.noreply.github.com>
2021-11-13 11:02:21 +0800

    Update 列表去重.py

--- python/week 4/列表去重.py
+++ python/week 4/列表去重.py
@@ -1,3 +1,19 @@
+# 【问题描述】输入一系列以逗号分隔的英文人名，其中包含重复的名字，请将其中重复的名字去掉，输出包含不重复人名的列表，名字出现顺序与输入顺序相同。【输入形式】
+
+# 输入一系列以逗号分隔的英文人名，其中包含重复的名字，请将其中重复的名字去掉，输出包含不重复人名的列表，名字出现顺序与输入顺序相同。一系列以逗号分隔的英文人名
+
+# 【输出形式】
+
+# 包含不重复人名的列表，名字出现顺序与输入顺序相同
+# 【样例输入】
+
+# Calvin,bob,ada,McCord,Smith,Babbs,Calvin,Smith
+# 【样例输出】
+
+# ['Calvin', 'bob', 'ada', 'McCord', 'Smith', 'Babbs']
+
+
+
 Name_list = input().split(',')
 Name_set = set(Name_list)
 Name_list2 = list(Name_set)
