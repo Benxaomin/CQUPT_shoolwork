e97779e894c82b7bb66eb1585a158dbedae67d98
renxiao <92198500+Benxaomin@users.noreply.github.com>
2021-11-13 11:04:20 +0800

    Update 求均值和大于均值的值.py

--- python/week 5/求均值和大于均值的值.py
+++ python/week 5/求均值和大于均值的值.py
@@ -1,3 +1,22 @@
+# 【问题描述】
+
+# 编写一个函数cacluate, 可以接收任意多个数, 返回的是一个元组.元组的第一个值为所有参数的平均值（均值保留一位小数）, 第二个值是大于平均值的所有值
+
+# 【输入形式】
+# 【输出形式】
+# 【样例输入】
+
+# Please input numbers,and press the Enter to end.(gap with  )
+
+# 1,2,3,4,5
+
+
+# 【样例输出】
+
+# (3.0, [4, 5])
+
+
+
 def calculate(*b):
     b = input(
         'Please input numbers,and press the Enter to end.(gap with ,)\n').split(',')
