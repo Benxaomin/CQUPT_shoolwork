a8446168c5dde8896bcbd45bb27e56b74cedddd6
renxiao <92198500+Benxaomin@users.noreply.github.com>
2021-11-13 11:01:06 +0800

    Update 反复猜数2.py

--- python/week 4/反复猜数2.py
+++ python/week 4/反复猜数2.py
@@ -1,3 +1,39 @@
+# 【问题描述】
+
+# 用异常处理改写猜数游戏程序，功能是：允许用户反复输入数，直至猜中程序选定的数（假定为100）。输入的数如果大于选定的数，则提示"larger than expected"；如果小于选定的数，则提示"less than expected"；如果用户输入的不是整数，则提示"input error"；如果等于选定的数，则输出"you have tried N times, you win"并结束程序。
+# 【输入形式】
+
+# 一次或多次输入整数
+# 【输出形式】
+
+# 对于每一次输入，新起一行输出对于猜数结果的提示。
+
+# 【样例输入】
+
+# 50
+
+# 150
+
+# E
+
+# 100
+
+# 【样例输出】
+
+# less than expected
+
+# larger than expected
+
+# input error
+
+# you have tried 4 times, you win
+
+# 【说明】
+
+# 被猜的数设定为100。
+
+
+
 m = 100
 c = True
 count = 0
